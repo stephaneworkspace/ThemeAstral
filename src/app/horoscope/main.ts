bc01dd772c985c9e8f291a8bd813bfe36ba0679c
Stéphane <s.bressani@bluewin.ch>
2018-12-26 15:47:48 +0100

    backup

--- src/app/horoscope/main.ts
+++ src/app/horoscope/main.ts
@@ -1,10 +1,11 @@
-import { Calc } from "./calc"
+import { Calc } from './calc'
 import { drawer } from './drawer';
 
 export class Horoscope {
     private _drawer;
     private _properties;
     private validPlanetProperties;
+    private validHousesAxesProperties;
 
     constructor(properties) {
         // this._elements = elements;
@@ -26,16 +27,13 @@ export class Horoscope {
             'mars'
         ];
 
-        /*
-        // Maisons à faire, pour le moment en chantier
         this.validHousesAxesProperties = [
-            "axis2to8",
-            "axis3to9",
-            "axis4to10",
-            "axis5to11",
-            "axis6to12"
-          ];
-        */
+            'axis2to8',
+            'axis3to9',
+            'axis4to10',
+            'axis5to11',
+            'axis6to12'
+        ];
 
         // Chargement des ephemerides
         if (properties) {
@@ -47,8 +45,8 @@ export class Horoscope {
     }
 
     /**
-    * Sets default properties if no properties had been passed via constructor parameter.
-    */
+     * Sets default properties if no properties had been passed via constructor parameter.
+     */
     setDefaultProperties() {
         this._properties = {};
         this._properties.zodiac = {};
@@ -71,11 +69,89 @@ export class Horoscope {
         this._properties.houses = {};
         this._properties.houses.hasHouses = true;
         this._properties.houses.axes = {};
-        /*
+
         this._properties.houses.axes.axis2to8 = Calc.getRandomArbitrary(25, 40);
         this._properties.houses.axes.axis3to9 = Calc.getRandomArbitrary(50, 60);
         this._properties.houses.axes.axis4to10 = Calc.getRandomArbitrary(75, 85);
         this._properties.houses.axes.axis5to11 = Calc.getRandomArbitrary(110, 120);
-        this._properties.houses.axes.axis6to12 = Calc.getRandomArbitrary(150, 160);*/
-  }
+        this._properties.houses.axes.axis6to12 = Calc.getRandomArbitrary(150, 160);
+    }
+
+    /**
+     * Validates the properties of the horoscope. Usually, they are passed through the constructor argument.
+     * @param properties propriété
+     */
+    validateProperties(properties) {
+        this.validateZodiac(properties);
+        this.validatePlanets(properties);
+        this.validateHouses(properties);
+    }
+
+    /**
+     * @param properties propriété
+     */
+    validateZodiac(properties) {
+        if (!properties.hasOwnProperty('zodiac')) {
+            throw new Error('No "zodiac" property set for horoscope.');
+        }
+        if (!properties.zodiac.hasOwnProperty('ascendant')) {
+            throw new Error('The "zodiac" property requires "ascendant" property to be set.');
+        }
+        if (!properties.zodiac.ascendant.hasOwnProperty('sign') || !properties.zodiac.ascendant.hasOwnProperty('degree')) {
+            throw new Error('The "zodiac.ascendant" property requires "sign" and "degree" property to be set.');
+        }
+    }
+
+    /**
+     * @param properties propriété
+     */
+    validatePlanets(properties) {
+        const validPlanets = this.validPlanetProperties;
+
+        if (!properties.hasOwnProperty('planets')) {
+            throw new Error('No "planets" property set for horoscope.');
+        }
+        if (Object.keys(properties.planets).length <= 0) {
+            throw new Error('The "planets" property requires at least one planet to be set.');
+        }
+        const invalidPlanets = Object.keys(properties.planets).filter(elem => {
+        if (!validPlanets.includes(elem)) {
+            return true;
+        }
+        return false;
+        });
+        if (invalidPlanets.length > 0) {
+            throw new Error('The "planets" property has invalid planets.');
+        }
+    }
+
+    validateHouses(properties) {
+        const validHousesAxes = this.validHousesAxesProperties;
+
+        if (!properties.hasOwnProperty('houses')) {
+          throw new Error('The "houses" is required.');
+        }
+        if (!properties.houses.hasOwnProperty('hasHouses')) {
+          throw new Error('The "houses" property requires "hasHouses" property to be set.');
+        }
+        if (typeof(properties.houses.hasHouses) !== 'boolean') {
+          throw new Error('The "houses" property requires "hasHouses" property to be of type boolean set.');
+        }
+        if (properties.houses.hasHouses) {
+          if (!properties.houses.hasOwnProperty('axes')) {
+            throw new Error('No "axes" property set for horoscope.');
+          }
+          if (Object.keys(properties.houses.axes).length !== validHousesAxes.length) {
+            throw new Error('A horoscope with "houses" requires exactly ' + validHousesAxes.length + ' axes to be set.');
+          }
+          const invalidHousesAxes = Object.keys(properties.houses.axes).filter(elem => {
+            if (!validHousesAxes.includes(elem)) {
+              return true;
+            }
+            return false;
+          });
+          if (invalidHousesAxes.length > 0) {
+            throw new Error('The "houses" property has invalid axes set.');
+          }
+        }
 }
